5716572e80dbcfb306c5f46288719c00c5125f5c
ctyanphat <150544400+ctyanphat@users.noreply.github.com>
2023-11-13 23:04:57 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://phutungxenang.com'>https://phutungxenang.com</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.sieuthiphutungxenang.com/'>https://www.sieuthiphutungxenang.com/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
